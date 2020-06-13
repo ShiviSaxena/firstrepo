581570f993f03733468ce1a6af6f5ec0dc0ffbfb
ShiviSaxena <66834324+ShiviSaxena@users.noreply.github.com>
2020-06-13 15:45:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <body bgcolor="grey">
 <pre>
 <?php
-print "Welcome to task1..!!/n";
-print "Linux World.!/n";
-print "Biggest knowledge platform./n";
+print "Welcome to task1..!!\n";
+print "Linux World.!\n";
+print "Biggest knowledge platform.\n";
 
-print `ifconfig/n`;
-print "HELLO , HOW ARE YOU../n";
-print "Stay home.!!/n";
+print `ifconfig\n`;
+print "HELLO , HOW ARE YOU..\n";
+print "Stay home.!!\n";
 
 
 ?>
