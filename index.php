ccf43a84c17872faf49b9114a1263331f8f92293
ShiviSaxena <66834324+ShiviSaxena@users.noreply.github.com>
2020-06-13 21:39:38 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,9 @@ print "Biggest knowledge platform.\n";
 print `ifconfig\n`;
 print "HELLO , HOW ARE YOU..\n";
 print "Stay home.!!\n";
+<html>
+  <img scr="d1mrsjee7quv7u.cloudfront.net/aws terraform.png  height="200"  width="200">
+</html>
 
 ?>
 </pre>
