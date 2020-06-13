d00677392bac4564d556d2931de78389ef182a2d
ShiviSaxena <66834324+ShiviSaxena@users.noreply.github.com>
2020-06-13 23:41:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <body bgcolor="grey">
 <pre>
+<img src="/aws terraform.png" width="100" height="100" >
+
 <?php
 print "Welcome to task1..!!\n";
 print "Linux World.!\n";
@@ -8,11 +10,7 @@ print "Biggest knowledge platform.\n";
 print `ifconfig\n`;
 print "HELLO , HOW ARE YOU..\n";
 print "Stay home.!!\n";
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
-<html>
-<img src="terraform.png" width="500" height="300" title="Terraform" alt="Terraform" />
-</html>
-';
+
 
 ?>
 </pre>
